292b2ec7fa767f50cca50b8c0a77baf12275f989
Marcos Hiram Contreras Mendoza <162393647+MarcosHiramContreras@users.noreply.github.com>
2024-04-16 09:24:44 -0600

    Add files via upload

---
+++ PROYECTO SECUENCIA 1 CONTRERAS MENDOZA MARCOS HIRAM 174155 (1)/sqlite-panini/README.md
@@ -0,0 +1,12 @@
+# Ejemplo básico de PHP
+
+## Instalar
+
+1. Bajar esta carpeta y almacenarla en su equipo en la ruta:
+"C:\xampp\htdocs"
+
+2. Arrancar el servidor Apache de XAMP
+
+3. Ejecutar poniendo la ruta :"http://localhost/sqlite-alumnos/php/leer-alumnos.php" en su navegador
+
+4. Probarlo usando ThunderClient de VSODE
---
+++ PROYECTO SECUENCIA 1 CONTRERAS MENDOZA MARCOS HIRAM 174155 (1)/sqlite-panini/php/insertar-producto.php
@@ -0,0 +1,48 @@
+<?php
+include 'vars.php';
+
+#verificar si vienen losparametros requeridos
+if (empty($_POST["nombre"])) {
+    http_response_code(400);
+	exit("Falta insertar el nombre"); #Terminar el script definitivamente
+}
+
+if (empty($_POST["categoria"])) {
+    http_response_code(400);
+	exit("falta insertar la categoria"); #Terminar el script definitivamente
+}
+if (empty($_POST["descripcion"])) {
+    http_response_code(400);
+	exit("falta insertar la descripcion"); #Terminar el script definitivamente
+}
+if (empty($_POST["stock"])) {
+    http_response_code(400);
+	exit("falta insertar el stock"); #Terminar el script definitivamente
+}
+if (empty($_POST["precio"])) {
+    http_response_code(400);
+	exit("falta insertar el precio"); #Terminar el script definitivamente
+}
+//
+$conex = new PDO("sqlite:" . $nombre_fichero);
+$conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$producto=[
+    "nombre"=> $_POST["nombre"],
+    "categoria"=> $_POST["categoria"],
+    "descripcion"=> $_POST["descripcion"],
+    "stock"=> $_POST["stock"],
+    "precio"=> $_POST["precio"]
+];
+try{
+    # preparando la consulta
+    $sentencia = $conex->prepare("insert into productos(nombre, categoria, descripcion, stock, precio) values(:nombre, :categoria, :descripcion, :stock, :precio);");
+    $resultado = $sentencia->execute($producto);
+    http_response_code(200);
+    echo "datos insertados";
+
+}catch(PDOException $exc){
+    http_response_code(400);
+    echo "Lo siento, ocurrió un error:".$exc->getMessage();
+}
+
+?>
